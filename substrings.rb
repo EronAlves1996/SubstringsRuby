4ddf9ff9705b653c40dfb89eb014300b6f22d62f
Eron Alves <eron.a.silva@capgemini.com>
2023-01-05 14:43:54 -0300

    Make function that pass the first test

--- substrings.rb
+++ substrings.rb
@@ -1,2 +1,6 @@
+def substrings(string, dictionary)
+  dictionary.reduce(Hash.new(0)) { |result, word| result[word] += 1 if string.include?(word); result }
+end
+
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 puts substrings("below", dictionary)
